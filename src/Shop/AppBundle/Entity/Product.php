a777af273c7cb0beb57a814f0538564b48699927
Roman Ismailov <benny-krid@mail.ru>
2015-03-27 11:58:16 +0300

    add files

--- src/Shop/AppBundle/Entity/Product.php
+++ src/Shop/AppBundle/Entity/Product.php
@@ -8,7 +8,37 @@
 
 namespace Shop\AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name=product)
+ *
+ * Class Product
+ * @package Shop\AppBundle\Entity
+ */
+
 class Product
 {
+    /**
+     * @Id
+     * @Column(name="id", type="integer")
+     * @GeneratedValue
+     */
+    protected $id;
 
+    /**
+     * @Column(type="string")
+     */
+    protected $product;
+
+    /**
+     * @Column(type="decimal", scale=2)
+     */
+    protected $price;
+
+    /**
+     * @Column(type="integer")
+     */
+    protected $amount;
 }
